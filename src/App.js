b442d271804162c555e2487b6dfc2b67e39f55fc
mukul <mukulupadhyay073@gmail.com>
2022-07-19 22:12:28 +0530

    first commit

---
+++ src/App.js
@@ -0,0 +1,18 @@
+import 'bootstrap/dist/css/bootstrap.min.css';
+import './App.css';
+import {useState} from 'react';
+import { Container, Navbar, Row, Col } from 'react-bootstrap';
+
+const App = () => {
+
+  
+
+  
+  return (
+    <div className="App">
+      
+    </div>
+  );
+}
+
+export default App;
